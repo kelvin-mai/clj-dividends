59af5c46d391523f4382a5b54a706407deec002f
Kelvin Mai <kelvin.mai002@gmail.com>
2021-10-24 18:19:04 -0700

    handle errors

--- server/src/dividends/router/exception.clj
+++ server/src/dividends/router/exception.clj
@@ -1,20 +1,39 @@
 (ns dividends.router.exception
   (:require [taoensso.timbre :as log]
+            [reitit.coercion :as coercion]
             [reitit.ring.middleware.exception :as exception]))
 
-(defn handle-exception [message exception request]
-  {:status 500
-   :body {:success false
-          :message message
-          :exception (.getClass exception)
-          :data (ex-data exception)
-          :uri (:uri request)}})
+(defn exception-response
+  [status message exception request]
+  {:status status
+   :body (merge {:success false
+                 :message message
+                 :uri (:uri request)}
+                (when exception
+                  {:exception (.getClass exception)
+                   :data (ex-data exception)}))})
+
+(defn handle-exception [status message]
+  (fn
+    ([request]
+     (exception-response status message nil request))
+    ([exception request]
+     (exception-response status message exception request))))
+
+(defn handle-coercion-exception [status message]
+  (fn [exception request]
+    (exception-response status
+                        message
+                        (coercion/encode-error (ex-data exception))
+                        request)))
 
 (def exception-middleware
   (exception/create-exception-middleware
-    (merge
-      exception/default-handlers
-      {::exception/default (partial handle-exception "default")
-       ::exception/wrap (fn [handler e request]
-                          (log/error e (:uri request))
-                          (handler e request))})))
+   {::exception/default (handle-exception 500 "Internal server error")
+    :muuntaja/decode (handle-exception 400 "Malformed request")
+    ::coercion/request-coercion (handle-coercion-exception 400 "Malformed request")
+    ::coercion/response-coercion (handle-coercion-exception 500 "Malformed response")
+    java.sql.SQLException (handle-exception 500 "Database exception")
+    ::exception/wrap (fn [handler e request]
+                       (log/error e (:uri request))
+                       (handler e request))}))
--- server/src/dividends/services/router.clj
+++ server/src/dividends/services/router.clj
@@ -5,7 +5,8 @@
             [muuntaja.core :as m]
             [reitit.coercion.spec :refer [coercion]]
             [dividends.api.core :as api]
-            [dividends.router.middleware :refer [global-middleware]]))
+            [dividends.router.middleware :refer [global-middleware]]
+            [dividends.router.exception :refer [handle-exception]]))
 
 (defmethod ig/init-key :reitit/routes
   [_ {:keys [db]}]
@@ -19,4 +20,7 @@
             :middleware global-middleware}})
    (ring/routes
     (ring/redirect-trailing-slash-handler)
-    (ring/create-default-handler))))
+    (ring/create-default-handler
+     {:not-found (handle-exception 404 "Route not found")
+      :method-not-allowed (handle-exception 405 "Method not allowed")
+      :not-acceptable (handle-exception 406 "Not acceptable")}))))
